Add more signature options, fix byte overflow bug

biocy/wrappers.py
@@ -13,8 +13,8 @@ def get_variant_signatures(obgraph, variant_to_nodes, counter, k=31):
     counter = Counter(counter._keys.ravel().copy(), counter._values.ravel().copy())
     counter._values = counter._values.astype(np.int64)
 
-    kmer_finder = KmerFinder(graph, k, max_variant_nodes=200)
-    kmer_finder.set_kmer_frequency_index(counter)
+    kmer_finder = KmerFinder(graph, k, max_variant_nodes=4)
+    #kmer_finder.set_kmer_frequency_index(counter)
 
     results = kmer_finder.find_variant_signatures(ref_nodes, var_nodes, reverse_kmers=True)
 